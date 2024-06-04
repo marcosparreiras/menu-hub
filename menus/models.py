7e572642c0d9a27ffeefc4d99c2b9c64316f97a1
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-03 22:15:54 -0300

    feat: add restaurant and menu-item models

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self) -> str:
+        return f"[Restaurant] name: {self.name}"
+
+
+class MenuItem(models.Model):
+    category = models.CharField(max_length=45)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name="itens"
+    )
+
+    def __str__(self):
+        """Retorna uma representação em string do objeto."""
+        return f"[MenuItem] description: {self.description[:50]}"
