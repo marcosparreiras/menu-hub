ae1565e5aaa1cc0316c1ca941cbf2c12a98d1e10
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-21 00:17:55 -0300

    feat: add edit-restaurant view

--- menus/urls.py
+++ menus/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.show_restaurants, name="home"),
+    path("edit/<int:restaurante_id>", views.edit_restaurants, name="edit"),
 ]
--- menus/views.py
+++ menus/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant, MenuItem
 
 
-# Create your views here.
 def show_restaurants(request):
     restaurants = Restaurant.objects.order_by("name")
     context = {"restaurants": restaurants}
     return render(request, "menus/show_restaurants.html", context)
+
+
+def edit_restaurants(request, restaurant_id):
+    restaurant = Restaurant.objects.get(pk=restaurant_id)
+    if request.method == "POST":
+        form_data = request.POST
+        restaurant.name = form_data.get("name")
+        restaurant.save()
+        return redirect("/")
+
+    context = {"restaurant": restaurant}
+    return render(request, "menus/edit_restaurant.html", context)
