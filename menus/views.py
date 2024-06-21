5e90d9018d3473f0b194452fafa3dc176df80dec
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-20 23:40:53 -0300

    feat: add show-restaurants view

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Restaurant, MenuItem
+
 
 # Create your views here.
+def show_restaurants(request):
+    restaurants = Restaurant.objects.order_by("name")
+    context = {"restaurants": restaurants}
+    return render(request, "menus/show_restaurants.html", context)
